8d5c3f1ed48f647fece2134e83cdd1b81704c0ac
ZCC98GDLMX <cristhianzepeda98@gmail.com>
2026-05-24 21:38:18 -0600

    Add kill reward endpoint

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes";
 import playerRoutes from "./routes/player.routes";
+import rewardRoutes from "./routes/reward.routes";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/player", playerRoutes);
+app.use("/rewards", rewardRoutes);
 
 app.get("/health", (_req, res) => {
   return res.status(200).json({
