a8a4b0cef1a5f3bd9490a11b911cc99730b32478
ZCC98GDLMX <cristhianzepeda98@gmail.com>
2026-05-25 20:38:37 -0600

    Add remaining normal map NPC rewards

--- src/game/npcRewards.ts
+++ src/game/npcRewards.ts
@@ -45,6 +45,52 @@ export const NPC_REWARDS: Record<string, {
   pearls: 180,
   crystals: 1,
   },
+
+  RedKorsar1: { xp: 2, gold: 11000, pearls: 0, crystals: 0 },
+  RedKorsar2: { xp: 2, gold: 11000, pearls: 0, crystals: 0 },
+  RedKorsar3: { xp: 2, gold: 11000, pearls: 0, crystals: 0 },
+
+  LosRenegados1: { xp: 6, gold: 41000, pearls: 0, crystals: 0 },
+  LosRenegados2: { xp: 6, gold: 41000, pearls: 0, crystals: 0 },
+  LosRenegados3: { xp: 6, gold: 41000, pearls: 0, crystals: 0 },
+
+  Wild1: { xp: 10, gold: 66500, pearls: 0, crystals: 0 },
+  Wild2: { xp: 10, gold: 66500, pearls: 0, crystals: 0 },
+  Wild3: { xp: 10, gold: 66500, pearls: 0, crystals: 0 },
+
+  Tortuga1: { xp: 13, gold: 59000, pearls: 0, crystals: 0 },
+  Tortuga2: { xp: 13, gold: 59000, pearls: 0, crystals: 0 },
+  Tortuga3: { xp: 13, gold: 59000, pearls: 0, crystals: 0 },
+
+  Sinclair1: { xp: 16, gold: 120000, pearls: 0, crystals: 0 },
+  Sinclair2: { xp: 16, gold: 120000, pearls: 0, crystals: 0 },
+  Sinclair3: { xp: 16, gold: 120000, pearls: 0, crystals: 0 },
+
+  Ratpack1: { xp: 18, gold: 136500, pearls: 0, crystals: 0 },
+  Ratpack2: { xp: 18, gold: 136500, pearls: 0, crystals: 0 },
+  Ratpack3: { xp: 18, gold: 136500, pearls: 0, crystals: 0 },
+
+  CalicosMen: { xp: 98, gold: 208500, pearls: 0, crystals: 0 },
+  LosAmistadores: { xp: 18, gold: 156500, pearls: 0, crystals: 0 },
+  MorganBucanner: { xp: 117, gold: 267000, pearls: 0, crystals: 0 },
+  FlyingDutchman: { xp: 1965, gold: 0, pearls: 550, crystals: 0 },
+  Tiamat: { xp: 5100, gold: 0, pearls: 1000, crystals: 5 },
+
+  Kilimatu: { xp: 173, gold: 0, pearls: 170, crystals: 1 },
+  Kiliwallis: { xp: 258, gold: 0, pearls: 270, crystals: 2 },
+  Kiribati: { xp: 118, gold: 0, pearls: 130, crystals: 1 },
+  Kokelau: { xp: 630, gold: 0, pearls: 455, crystals: 3 },
+
+  Trinidad: { xp: 55, gold: 313000, pearls: 0, crystals: 0 },
+  SantaLucia: { xp: 180, gold: 417000, pearls: 0, crystals: 0 },
+  Esmeralda: { xp: 210, gold: 530000, pearls: 0, crystals: 0 },
+
+  Almirante: { xp: 17850, gold: 0, pearls: 3400, crystals: 10 },
+  AdmiralSinclair: {  xp: 3200,  gold: 0,  pearls: 4800,  crystals: 2,},
+  AdmiralCalico: { xp: 4180, gold: 0, pearls: 6900, crystals: 4 },
+  AdmiralKiribati: { xp: 7600, gold: 0, pearls: 14200, crystals: 6 },
+  AdmiralLucia: { xp: 17100, gold: 0, pearls: 20000, crystals: 8 },
+
 };
 
 export const MONSTER_REWARDS: Record<string, {
